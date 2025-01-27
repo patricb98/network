8cbabc6457831156ded165660101d4cb614b8ced
Patric Beaven <patricbeaven@outlook.com>
2025-01-27 16:01:05 +0800

    set up models and post serialiser

--- network/models.py
+++ network/models.py
@@ -3,4 +3,21 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    following = models.ManyToManyField(
+        'self',
+        symmetrical=False,
+        related_name='followers',
+        blank=True
+    )
+    
+    def __str__(self):
+        return self.username
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True) 
+    likes = models.ManyToManyField(User, blank=True, related_name='liked_posts') 
+
+    def __str__ (self):
+        return f"{self.user.username}: {self.content[:30]}: {self.timestamp}"
--- project4/settings.py
+++ project4/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'network',
     'django.contrib.admin',
     'django.contrib.auth',
